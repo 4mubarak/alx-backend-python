c9756f0ecdd58436c984dcf9432d326107ffbad5
4mubarak <muber021@gmail.com>
2024-03-18 16:48:03 +0000

    Task4 task_wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,20 +1,15 @@
-
 #!/usr/bin/env python3
-"""
-A module for creating and running multiple asyncio Tasks concurrently.
-"""
+""" Module documentation """
 import asyncio
+import random
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+
+task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Create and run `n` asyncio Tasks that wait for random amounts of time
-    up to `max_delay` seconds. Returns a list of the Task objects, sorted
-    in ascending order by the time waited.
-    """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """doc func"""
+    listDelays = []
+    for _ in range(n):
+        listDelays.append(task_wait_random(max_delay))
+    return sorted(await asyncio.gather(*listDelays))
